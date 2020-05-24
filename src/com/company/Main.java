80712a1c93b009d6442bee7f446cb22dd1af022f
Kotes <51745529+KotesUA@users.noreply.github.com>
2020-05-24 21:34:58 +0300

    Chage

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,20 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        int currentPrice = 5;
-        CardCatalog cc = new CardCatalog(currentPrice);
-
-        MoneyCard mc = new MoneyCard(false, 10);
-        MoneyCard mcp = new MoneyCard(true, 10);
-        TimesCard tc = new TimesCard(false, 2);
-        TermCard trc = new TermCard(false, 3);
-
-        Turnstile turnstile = new Turnstile();
-
-        turnstile.Pass(mc, currentPrice, cc);   //must not work
-
-        cc.addCard(mc);
-
-        turnstile.Pass(mc, currentPrice, cc);   //works
+        
     }
 }
